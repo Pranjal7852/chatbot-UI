f0d6387a1a9bbfb4b2540af4aec3ba1942d38c4b
Pranjal Goyal <id4pranjal@gmail.com>
2024-05-10 16:36:15 +0530

    added routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import impala from "@impalajs/core/plugin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), impala()],
 });
